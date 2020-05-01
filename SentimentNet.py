26b732a4abc06d208d34e9549adb3250b6594367
nitishtalasu <talasunitish@gmail.com>
2020-05-01 13:57:24 -0500

    Add files via upload

---
+++ SentimentNet.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from CustomLstm import *
+# from lstm import *
+
+class SentimentNet(nn.Module):
+    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, drop_prob=0.5):
+        super(SentimentNet, self).__init__()
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        # self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=drop_prob, batch_first=True)
+        self.lstm = LSTM(cell_class=LSTMCell, input_size=embedding_dim, hidden_size=hidden_dim, num_layers = n_layers ,dropout=drop_prob, batch_first=True)
+        # self.lstm = LSTM(embedding_dim, hidden_dim, n_layers)
+        self.dropout = nn.Dropout(drop_prob)
+        self.fc = nn.Linear(hidden_dim, output_size)
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x, hidden):
+        batch_size = x.size(0)
+        x = x.long()
+        embeds = self.embedding(x)
+        lstm_out, hidden = self.lstm(embeds, hidden)
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+        
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        out = self.sigmoid(out)
+        
+        out = out.view(batch_size, -1)
+        out = out[:,-1]
+        return out, hidden
+    
+    def init_hidden(self, batch_size):
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        else:
+            device = torch.device("cpu")
+        weight = next(self.parameters()).data
+        hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(device),
+                      weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(device))
+        return hidden
